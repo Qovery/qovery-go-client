ca35a71cc88649a0a53c81eec18f2a1144100436
Rémi Demol <rde@sirdata.fr>
2020-01-13 12:39:38 +0100

    fix property name FQDN

--- configuration_database.go
+++ configuration_database.go
@@ -3,7 +3,7 @@ package qovery
 type DatabaseConfiguration struct {
 	Type     string `json:"type"`
 	Name     string `json:"name"`
-	Host     string `json:"host"`
+	FQDN     string `json:"fqdn"`
 	Port     int    `json:"port"`
 	Username string `json:"username"`
 	Password string `json:"password"`
